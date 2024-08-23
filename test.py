c527ad07baa2fd0fe1c9c9ba3a148a5d31d8ebe7
megahomyak <g.megahomyak@gmail.com>
2024-08-24 01:11:52 +0500

    THIS IS DONE.

--- test.py
+++ test.py
@@ -1,47 +1,57 @@
 from dataclasses import dataclass
 
 @dataclass
+class BasedNumber:
+    digits: list[int]
+    base: int
+
+    def convert(self, new_base):
+        self_as_number = 0
+
+        for index, digit in enumerate(self.digits[::-1]):
+            self_as_number += self.base ** index * digit
+
+        reversed_digits = []
+        while self_as_number:
+            reversed_digits.append(int(self_as_number % new_base))
+            self_as_number //= new_base
+
+        self.base = new_base
+        self.digits = reversed_digits[::-1]
+
+@dataclass
 class BasedDigit:
     value: int
     base: int
 
-@dataclass
-class BinaryString:
-    bits: list[int]
+Bit = int
+Base = int
 
-class BasedNumber:
-    def __init__(self, initial_digits: list[int], base: int):
-        # I don't want these values to change independently
-        self._reversed_digits = initial_digits[::-1]
-        self._base = base
+def encode(values: list[int], bases: list[Base]) -> list[Bit]:
+    final_number = BasedNumber(digits=[1], base=2)
+    for value, base in zip(values, bases):
+        final_number.convert(new_base=base)
+        final_number.digits.append(value)
+    final_number.convert(new_base=2)
+    final_number.digits.pop(0) # Leftmost "1"
+    return final_number.digits
 
-    @property
-    def digits(self):
-        return self._reversed_digits[::-1]
+def decode(encoded_values: list[Bit], bases: list[Base]) -> list[int]:
+    final_number = BasedNumber(digits=[1] + encoded_values, base=2)
+    values = []
+    for base in bases[::-1]:
+        final_number.convert(base)
+        values.append(final_number.digits.pop())
+    return values[::-1]
 
-    @property
-    def base(self):
-        return self._base
+def main():
+    bases = [Base(20), Base(3), Base(2)]
+    print(f"{bases = }")
+    values = [18, 2, 1]
+    print(f"{values = }")
+    encoded_values = encode(values, bases)
+    print(f"{encoded_values = }")
+    decoded_values = decode(encoded_values, bases)
+    print(f"{decoded_values = }")
 
-    def convert(self, new_base: int) -> "BasedNumber":
-        self_as_number = 0
-
-        for index, digit in enumerate(self._reversed_digits):
-            self_as_number += self.base ** index * digit
-
-        digits = []
-        while self_as_number:
-            digits.append(int(self_as_number % new_base))
-            self_as_number //= new_base
-
-        return BasedNumber(initial_digits=digits[::-1])
-
-
-class Base(int): pass
-
-def encode(digits: list[BasedDigit]) -> BinaryString:
-    base = 2
-    output_digits = [1]
-    for digit in digits:
-        
-    return output_digits
+main()
