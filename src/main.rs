49226f6dfc59bde8f5bdf4cc99080e9c7bb8e2da
megahomyak <g.megahomyak@gmail.com>
2024-09-09 23:46:17 +0500

    Brought the types back

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 mod number;
 use num_bigint::BigUint;
 
-use number::{Readable, Writeable};
+use number::{Base, Digit, Readable, Writeable};
 
 fn main() {
     fn biguint(value: u32) -> BigUint {
         BigUint::new(vec![value])
     }
+    fn base(value: u32) -> Base {
+        Base::new(biguint(value)).unwrap()
+    }
+    fn digit(value: u32, base: Base) -> Digit {
+        Digit::new(biguint(value), base).unwrap()
+    }
     let mut number = BigUint::ZERO.clone();
-    number.write(&biguint(15), &biguint(20));
-    number.write(&biguint(2), &biguint(3));
-    number.write(&biguint(1), &biguint(2));
-    println!("{}", number.read(&biguint(2)));
-    println!("{}", number.read(&biguint(3)));
-    println!("{}", number.read(&biguint(20)));
+    number.write(&digit(15, base(20)));
+    number.write(&digit(2, base(3)));
+    number.write(&digit(1, base(2)));
+    println!("{}", number.read(&base(2)));
+    println!("{}", number.read(&base(3)));
+    println!("{}", number.read(&base(20)));
 }
--- src/number.rs
+++ src/number.rs
@@ -1,23 +1,50 @@
 use num_bigint::BigUint;
 
+pub struct Base {
+    value: BigUint,
+}
+impl Base {
+    pub fn new(value: BigUint) -> Option<Self> {
+        if value > BigUint::ZERO {
+            Some(Self { value })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Digit {
+    base: Base,
+    value: BigUint,
+}
+impl Digit {
+    pub fn new(value: BigUint, base: Base) -> Option<Self> {
+        if base.value > value {
+            Some(Self { base, value })
+        } else {
+            None
+        }
+    }
+}
+
 pub trait Readable {
-    fn read(&mut self, limit: &BigUint) -> BigUint;
+    fn read(&mut self, base: &Base) -> BigUint;
 }
 
 impl Readable for BigUint {
-    fn read(&mut self, limit: &BigUint) -> BigUint {
-        let output = &*self % limit;
-        *self /= limit;
+    fn read(&mut self, base: &Base) -> BigUint {
+        let output = &*self % &base.value;
+        *self /= &base.value;
         output
     }
 }
 
 pub trait Writeable {
-    fn write(&mut self, value: &BigUint, limit: &BigUint);
+    fn write(&mut self, digit: &Digit);
 }
 
 impl Writeable for BigUint {
-    fn write(&mut self, value: &BigUint, limit: &BigUint) {
-        *self = &*self * limit + value;
+    fn write(&mut self, digit: &Digit) {
+        *self = &*self * &digit.base.value + &digit.value;
     }
 }
